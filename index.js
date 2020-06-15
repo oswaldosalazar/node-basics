8c860d6820877fe3d27720a66175d57a2b55bdc5
oswaldosalazar <oswaldo.r.salazar@gmail.com>
2020-06-14 18:36:40 -0600

    Finished threads

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
-// I'm a child, I'm going to act like a server
-// and do nothing else
 const express = require('express');
 const crypto = require('crypto');
 const app = express();
+const { Worker, parentPort } = require('worker_threads');
 
-// function doWork(duration) {
-//   const start = Date.now();
-//   while (Date.now() - start < duration) {}
-// }
-
-app.get('/', (req, res) => {
-  // doWork(5000);
-  crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
-    res.send('Hi there');
+app.get('/', (req, res, next) => {
+  const worker = new Worker('./worker.js');
+  worker.on('message', data => {
+    res.send(data);
   });
 });
 
@@ -20,4 +14,6 @@ app.get('/fast', (req, res) => {
   res.send('This was fast!');
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log('App up at: 3000');
+});
